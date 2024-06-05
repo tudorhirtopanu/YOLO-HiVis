77616dd2907a639b4a518377712911429723348c
Tudor Hirtopanu <122214687+tudorhirtopanu@users.noreply.github.com>
2024-06-05 15:43:11 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from track import track_people_in_hi_vis
 
-model_dir = '../models/HiVisModel.pt'
+model_dir = '.models/HiVisModel.pt'
 
 if __name__ == '__main__':
     track_people_in_hi_vis(model_dir)
